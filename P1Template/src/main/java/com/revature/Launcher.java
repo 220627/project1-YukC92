33282308d8d930dda06b3884a453f409f5bf77f3
YukC92 <yukfaichan92@gmail.com>
2022-07-25 23:51:23 -0700

    add controllers into launcher

--- P1Template/src/main/java/com/revature/Launcher.java
+++ P1Template/src/main/java/com/revature/Launcher.java
@@ -1,8 +1,37 @@
 package com.revature;
 
+import com.revature.controllers.AuthController;
+import com.revature.controllers.ReimbController;
+import com.revature.controllers.RoleController;
+import com.revature.controllers.StatusController;
+import com.revature.controllers.TypeController;
+import com.revature.controllers.UserController;
+
+import io.javalin.Javalin;
+
 public class Launcher {
 
 	public static void main(String[] args) {
+		
+		Javalin app = Javalin.create(
+				
+				config -> {
+					config.enableCorsForAllOrigins(); 
+				}
+				
+				).start(3000); 
+		
+		AuthController ac = new AuthController();
+		
+		RoleController rc = new RoleController();
+		
+		ReimbController rec = new ReimbController();
+		
+		StatusController sc = new StatusController();
+		
+		TypeController tc = new TypeController();
+		
+		UserController uc = new UserController();
 		
 		//Welcome to P1! 
 		
